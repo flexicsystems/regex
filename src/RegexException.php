9d8526c1aeef21ef879bd66af6cab9d545cfd243
Hendrik Legge <admin@themepoint.de>
2022-11-01 14:49:58 +0100

    Run cs fixer

--- src/RegexException.php
+++ src/RegexException.php
@@ -1,21 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
 namespace Flexic\Regex;
 
-class RegexException extends \Exception
+final class RegexException extends \Exception
 {
     protected string $action;
 
     public function __construct(
         string $action,
-        string $message = "",
-        int $code = 500
+        string $message = '',
+        int $code = 500,
     ) {
         $this->action = $action;
         parent::__construct(
             $message,
             $code,
-            null
+            null,
         );
     }
 
