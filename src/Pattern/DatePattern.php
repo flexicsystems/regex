2a4a45b80e3b4377e98070dd9ec1261713b2dd81
Hendrik Legge <admin@themepoint.de>
2022-11-03 21:44:11 +0100

    Added patterns

---
+++ src/Pattern/DatePattern.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2022-2022 Flexic-Systems
+ *
+ * @author Hendrik Legge <hendrik.legge@themepoint.de>
+ *
+ * @version 1.0.0
+ */
+
+namespace Flexic\Regex\Pattern;
+
+use Flexic\Regex\AbstractPattern;
+use Flexic\Regex\Modifier\MultiLine;
+use Flexic\Regex\Modifier\SingleLine;
+
+final class DatePattern extends AbstractPattern
+{
+    public function __construct()
+    {
+        $this->setPattern('([0-9]{4})-([0-9]{2})-([0-9]{2})');
+        $this->setModifier([]);
+    }
+}
